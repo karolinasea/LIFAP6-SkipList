ccd910cc9cac51434119f91166744ac02b8c31ba
karolinasea <carolemerle0@gmail.com>
2020-02-17 01:05:16 +0100

    inserer() seems to work properly now

--- skipList.cpp
+++ skipList.cpp
@@ -6,8 +6,7 @@
 SkipList::SkipList() {
   bidon = new Cellule(); 
   taille = 0;
-  //std::cout << "taille = " << taille << std::endl;
-  //std::cout << "bidon->tab.size() = " << bidon->tab.size() << std::endl;
+  //std::cout << "constructor, taille = " << taille << std::endl;
 }
 
 SkipList::~SkipList() {
@@ -20,137 +19,181 @@ SkipList::~SkipList() {
 
 void SkipList::afficher() {
   Cellule *c = bidon;
-  for(int i=0; i<taille; i++) {
-  	// std::cout << "afficher 1st loop i = " << i << std::endl;
-    for(int j=c->tab.size()-1; j>0; j--) {
-      // std::cout << "afficher 2nd loop j = " << j << std::endl;
-      if(c->tab[j] != nullptr) {
-        std::cout << "pour la cellule c numero " << i << ", c->tab[" << j << "] pointe sur cellule de valeur = " << c->tab[j]->val << std::endl;
+  std::cout << "taille de la liste = " << taille << std::endl;
+  for(int i=0; i<=taille; i++) { //
+  	std::cout << "afficher 1st loop i = " << i << std::endl;
+  	// std::cout << "ATTENTION c->tab.size() = " << c->tab.size() << std::endl;
+    for(int j=0; j<c->tab.size(); j++) {
+      std::cout << "afficher 2nd loop j = " << j << std::endl;
+      if(c == bidon && c->tab[j] != nullptr) {
+      	std::cout << "La cellule bidon (i = " << i << "), bidon->tab[" << j << "] pointe sur cellule de valeur = " << c->tab[j]->val << std::endl;
+      } else if(c == bidon && c->tab[j] == nullptr) {
+      	std::cout << "La cellule bidon (i = " << i << "), bidon->tab[" << j << "] pointe sur nullptr" << std::endl;
+      } else if(c->tab[j] != nullptr) {
+        std::cout << "pour la cellule c numero " << i << " de val : " << c->val << ", c->tab[" << j << "] pointe sur cellule de valeur = " << c->tab[j]->val << std::endl;
       } else if(c->tab[j] == nullptr) {
-        std::cout << "pour la cellule c numero " << i << ", c->tab[" << j << "] pointe sur nullptr " << std::endl;
+        std::cout << "pour la cellule c numero " << i << " de val : " << c->val << ", c->tab[" << j << "] pointe sur nullptr " << std::endl;
       }
     }
+    // if(c->tab[0] == bidon->tab[0]) std::cout << "c->tab[0] == bidon->tab[0] TRUE and bidon->tab[0]->tab.size() = " << bidon->tab[0]->tab.size() << std::endl;
     c = c->tab[0];
   } 
-  /*while(c != nullptr) {
-    std::cout << c->val << std::endl;
-    c = c->tab[0];
-  }*/
 }
 
 void SkipList::inserer(int valeur) {
-  std::srand(std::time(nullptr));
-
   int t = 1;  
   if(std::rand()%2) t++;
-  // std::cout << "t = " << t << std::endl;
+  //t = t+std::rand()%6;
+  std::cout << "pour insert valeur = " << valeur << ", t = " << t << std::endl;
   Cellule *nouvelle = new Cellule(t, valeur);
-
-  //si la hauteur de la nouvelle cellule depasse la hauteur de la cellule bidon
+  // nouvelle->tab.resize(t);
+	std::cout << "hello 1" << std::endl;
 
   int h = bidon->tab.size(); 
-  // std::cout << "h = " << h << std::endl << std::endl;
-  // if(t>h) std::vector<Cellule *> update(t);
-  // else 
   std::vector<Cellule *> update;
   
+// if(t > h) {
+//   	std::cout << "hello 8" << std::endl;
+//   	bidon->tab.resize(t);
+//   	for(int i=h; i<t; i++) {
+//   		bidon->tab[i] = nouvelle;
+//   		std::cout << "hello 9" << std::endl;
+//   	}
+//   }
+
+	Cellule *c = bidon;
+
+
+
+	for(int i=bidon->tab.size()-1; i>=0; i--) {
+	  	std::cout << "hello 4" << std::endl;
+	    while((c->tab[i] != nullptr) && (c->tab[i]->val <= valeur)) {
+	    	// std::cout << "hello 5" << std::endl;
+	    	c = c->tab[i];
+	    	// update.push_back(c);
+	    	std::cout << "hello 6" << std::endl;
+	    }
+	    if(update.size() > 0) {
+		    if(update.back() != c) {
+		   		update.push_back(c);
+		   		std::cout << "hello 6.6" << std::endl;
+		   	}
+	    } else if(update.size() == 0) {
+		    std::cout << "hello 6.9" << std::endl;
+		   	update.push_back(c);
+	    }
+	    //update.push_back(c);
+	    std::cout << "hello 6.1" << std::endl;
+	  }
+	  
+	// for(int i=bidon->tab.size()-1; i>=0; i--) {
+	//   	std::cout << "hello 4" << std::endl;
+	//     while((c->tab[i] != nullptr) && (c->tab[i]->val <= valeur)) {
+	//     	// std::cout << "hello 5" << std::endl;
+	//     	c = c->tab[i];
+	//     	// update.push_back(c);
+	//     	std::cout << "hello 6" << std::endl;
+	//     }
+	//     std::cout << "hello 6.1" << std::endl;
+	//     // if(c == nullptr) {
+	//     // 	std::cout << "NULL PTR i = " << i << std::endl;
+	//     // }
+	//     if(c->tab.size() < nouvelle->tab.size()) {
+		    // if(update.size() > 0) {
+		    // 	if(update.back() != c) {
+		    // 		update.push_back(c);
+		    // 		std::cout << "hello 6.6" << std::endl;
+		    // 	}
+		    // } else if(update.size() == 0) {
+		    // 	std::cout << "hello 6.9" << std::endl;
+		    // 	update.push_back(c);
+		    // }
+	// 	}
+	//     //update.push_back(c);
+	//     // std::cout << "hello 7" << std::endl;
+	//   }
+
+	  std::cout << "update.size() = " << update.size() << std::endl;
+	  
+
+	// if(t > h) {
+	//   	std::cout << "hello 88" << std::endl;
+	//   	for(int i=h; i<t; i++) {
+	//   		bidon->tab.push_back(nouvelle);
+	//   		nouvelle->tab[i] = nullptr;
+	//   		std::cout << "hello 99" << std::endl;
+	//   	}
+ //  	}
+
+  std::cout << "hello 7.5" << std::endl;
+
+  // if(t > h) {
+  // 	std::cout << "hello 8" << std::endl;
+  // 	bidon->tab.resize(t);
+  // 	for(int i=h; i<t; i++) {
+  // 		bidon->tab[i] = nouvelle;
+  // 		std::cout << "hello 9" << std::endl;
+  // 	}
+  // }
   
-  // std::cout << "toto" << std::flush;
-  // Cellule *c = bidon;
-  // for(int i=bidon->tab.size()-1; i>0; i--) {
-  //   if(c->tab[i]->val != nullptr && c->tab[i]->val <= valeur) {
-  //      update->push_back(c->tab[i]);
-  //      c = c->tab[i];
-  //   }
+  // for(int i=0; i<bidon->tab.size(); i++) {
+  // 	std::cout << "hello 10" << std::endl;
+  // 	if(/*update[i]->tab.size()>i &&*/ (update[i]->tab[i] == nullptr || update[i]->tab[i]->val >= valeur)) {
+  // 		std::cout << "hello 11" << std::endl;
+  // 		nouvelle->tab[i] = update[i]->tab[i];
+  // 		std::cout << "hello 12" << std::endl;
+  // 		update[i]->tab[i] = nouvelle;
+  // 	} 
+  // 	std::cout << "hello 13" << std::endl;
   // }
 
-  // std::cout << "bidon->tab.size() = " << bidon->tab.size() << std::endl;
-  Cellule *c = bidon;
-
-  // if (c->tab[0] == nullptr) {
-  // 	std::cout << "NULLPTR  " << std::endl;
-  // }
-  // if (c->val == 0) {
-  // 	std::cout << "c->tab[i]->val == 0 " << std::endl;
-  // }
-
-	// for(int i=bidon->tab.size()-1; i>0; i--) {
-	//     std::cout << "hello  " << std::endl;
-	    // while((c->tab[i] != nullptr) && (c->val <= valeur)) {
-	    // 	c = c->tab[i];
-	    //   	std::cout << "c->tab[i]->val " << c->tab[i]->val << std::endl;
-	    // }
-	 //    update.push_back(c);
-	 //    std::cout << "hello 3 " << std::endl;
-  // }
-
-
-  for(int i=bidon->tab.size()-1; i>=0; i--) {
-    // std::cout << "i =  " << i << std::endl;
-    while((c->tab[i] != nullptr) && (c->tab[i]->val <= valeur)) {
-    	// std::cout << ".. i =  " << i << std::endl;
-    	c = c->tab[i];
-      	// std::cout << "c->tab[i]->val " << c->tab[i]->val << std::endl;
-    }
-    update.push_back(c);
-    // std::cout << "hello 3 " << std::endl;
+  for(int i=0; i<update.size(); i++) {
+  	std::cout << "hello 12 i = " << i << std::endl;
+  	for(int j=update[i]->tab.size()-1; j>=0; j--) {
+  		std::cout << "hello 12.12 j = " << j << std::endl;
+  		if(j < nouvelle->tab.size()) {
+  			if(update[i]->tab[j] == nullptr) {
+  				std::cout << "nullptr " << std::endl;
+  				nouvelle->tab[j] = update[i]->tab[j];
+		  		update[i]->tab[j] = nouvelle;
+  			} 
+  				//else {
+  			// 	std::cout << "update[i]->tab[j]->val = " << update[i]->tab[j]->val << std::endl;
+  			// }
+  			else if(update[i]->tab[j]->val <= nouvelle->val || j == 0 || nouvelle->tab.size() >= update[i]->tab.size()) {
+	  			nouvelle->tab[j] = update[i]->tab[j];
+		  		update[i]->tab[j] = nouvelle;
+		  		std::cout << "hello 11.11" << std::endl;
+		  	}
+  		}
+  		std::cout << "hello 11" << std::endl;
+  	}
   }
 
   if(t > h) {
-  	bidon->tab.resize(t);
-  	for(int i=h; i<t; i++) {
-  		bidon->tab[i] = nouvelle;
+	  	std::cout << "hello 88" << std::endl;
+	  	for(int i=h; i<t; i++) {
+	  		//bidon->tab.resize(t);
+	  		bidon->tab.push_back(nouvelle);
+	  		nouvelle->tab[i] = nullptr;
+	  		std::cout << "hello 99" << std::endl;
+	  	}
   	}
-  }
-  // std::cout << "apres check if t>h, h = " << bidon->tab.size() << std::endl << std::endl;
 
-  // std::cout << "update.size() = " << update.size() << std::endl;
-  // std::cout << "nouvelle->tab.size() = " << nouvelle->tab.size() << std::endl;
-
-  // std::cout << "nouvelle->afficher() = " << std::endl;
-  // nouvelle->afficher();
-  // std::cout << "bidon->afficher() = " << std::endl;
-  // bidon->afficher();
-  // nouvelle->tab[0] = update[0];
-  // std::cout << "update[0]->val = " << update[0]->val << std::endl;
-  
-  // std::cout << "update[0]->afficher() = " << std::endl;
-  // update[0]->afficher();
-  // std::cout << "nouvelle->tab[0]->afficher() = " << std::endl;
-  // nouvelle->tab[0]->afficher();
-
-
-	// std::cout << "talle de update should be is = " << update.size() << std::endl;
-	// if(update[0]->val == 0) {
-	// 	std::cout << "0000000000 " << std::endl;
-	// }
-	// if(update[0]->tab[0] == nullptr) {
-	// 	std::cout << "NULLPTR " << std::endl;
-	// } 
-  
-  for(int i=0; i<update.size(); i++) {
-  	// std::cout << "last loop i = " << i << std::endl;
-  	// std::cout << "update[i]->tab[i]->val = " << update[i]->tab[i]->val << std::endl;
-  	if(update[i]->tab[i] == nullptr || update[i]->tab[i]->val >= valeur) {
-  		// std::cout << "if with i = " << i << std::endl;
-  		nouvelle->tab[i] = update[i]->tab[i];
-  		update[i]->tab[i] = nouvelle;
-  	} 
-  }
- 
   taille++;
-  std::cout << "fin" << std::endl;
+  std::cout << "fin inserer(), taille = " << taille << std::endl;
 }
 
 bool SkipList::search(int valeur) {
   Cellule *c = bidon;
-  for(int i=bidon->tab.size()-1; i>0; i--)
-    while((c->tab[i] != nullptr) && (c->tab[i]->val <= valeur))
-      c = c->tab[i];
-  if(c->tab[0]->val == valeur)
-    return true;
-  else
-    return false;
+  for(int i=bidon->tab.size()-1; i>0; i--) {
+    while((c->tab[i] != nullptr) && (c->tab[i]->val <= valeur)) {
+      if(c->tab[i]->val == valeur) return true;
+      else c = c->tab[i];
+    }
+  }
+	//if(c->tab[0]->val == valeur) return true;
+	return false;
 }
 
 
--- testSkipList.cpp
+++ testSkipList.cpp
@@ -5,26 +5,63 @@
 
 int main() {
 
+	std::srand(std::time(nullptr));
 	SkipList s;
-	s.inserer(2);
-	// s.afficher();
-	s.inserer(4);
-	s.inserer(7);
+	// s.inserer(2);
 	s.inserer(13);
+	// s.afficher();
 	s.inserer(22);
+	
+	// s.afficher();
+	s.inserer(7);
+	s.inserer(4);
+	// s.afficher();
+	// s.inserer(88);
+	// s.afficher();
+	// s.inserer(5);
+	// s.inserer(8);
+	// s.afficher();
+	// // s.inserer(11);
+	// s.inserer(54);
+	// // s.inserer(1);
+
 	s.afficher();
 
-	if(s.search(7)) {
-		std::cout << "7 est dans la SkipList" << std::endl;
-	} else {
-		std::cout << "7 n'est pas dans la SkipList" << std::endl;
-	}
+	// if(s.search(7)) {
+	// 	std::cout << "7 est dans la SkipList" << std::endl;
+	// } else {
+	// 	std::cout << "FALSE 1" << std::endl;
+	// }
 
-	if(s.search(15)) {
-		std::cout << "15 est dans la SkipList" << std::endl;
-	} else {
-		std::cout << "15 n'est pas dans la SkipList" << std::endl;
-	}
+	// if(s.search(15)) {
+	// 	std::cout << "FALSE 2" << std::endl;
+	// } else {
+	// 	std::cout << "15 n'est pas dans la SkipList" << std::endl;
+	// }
+
+	// if(s.search(88)) {
+	// 	std::cout << "88 est dans la SkipList" << std::endl;
+	// } else {
+	// 	std::cout << "FALSE 3" << std::endl;
+	// }
+
+	// if(s.search(222)) {
+	// 	std::cout << "FALSE 4" << std::endl;
+	// } else {
+	// 	std::cout << "222 n'est pas dans la SkipList" << std::endl;
+	// }
+
+	// if(s.search(13)) {
+	// 	std::cout << "13 est dans la SkipList" << std::endl;
+	// } else {
+	// 	std::cout << "FALSE 5" << std::endl;
+	// }
+
+	// if(s.search(41)) {
+	// 	std::cout << "FALSE 6" << std::endl;
+	// } else {
+	// 	std::cout << "41 n'est pas dans la SkipList" << std::endl;
+	// }
 
 	return 0;
 }
